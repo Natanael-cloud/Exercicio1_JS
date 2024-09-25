a5af886d811423cc295964455e0299629d7fa1bb
natanaelcloud <natanpaivacosta@gmail.com>
2024-09-25 19:42:59 -0300

    criar váriavel palavra

---
+++ index.js
@@ -0,0 +1,8 @@
+// Palavra que deseja inserir
+let palavra = "Olá, Mundo!";
+
+// Selecionando o elemento no HTML
+let elemento = document.getElementById("display");
+
+// Exibindo a palavra no HTML
+elemento.innerText = palavra;
